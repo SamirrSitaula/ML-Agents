b56c360b1ed0fd30236071693c46bdf9efcb5eb8
Samir Sitaula <idsamirko@gmail.com>
2025-11-23 13:39:09 -0800

    Update backend url for render

--- my-best-bud-ml/src/frontend/app.py
+++ my-best-bud-ml/src/frontend/app.py
@@ -7,7 +7,7 @@ msg = st.text_area("Enter message")
 
 if st.button("Send"):
     # resp = requests.post("http://localhost:8000/chat", json={"message": msg}) #for local
-    BACKEND_URL = "BACKEND_URL = "https://ml-agents.onrender.com/chat" #updated url for render
+    BACKEND_URL = "https://ml-agents.onrender.com/chat" #updated url for render
     resp = requests.post(BACKEND_URL, json={"message": msg})
 
     st.json(resp.json())
