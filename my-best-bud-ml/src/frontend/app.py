c181d12044ca3a51576ff15bc5fb902586108158
Samir Sitaula <idsamirko@gmail.com>
2025-11-23 13:33:39 -0800

    Update backend URL to Render live link

--- my-best-bud-ml/src/frontend/app.py
+++ my-best-bud-ml/src/frontend/app.py
@@ -7,7 +7,7 @@ msg = st.text_area("Enter message")
 
 if st.button("Send"):
     # resp = requests.post("http://localhost:8000/chat", json={"message": msg}) #for local
-    BACKEND_URL = "https://ml-agent-backend.onrender.com/chat"
+    BACKEND_URL = "BACKEND_URL = "https://ml-agents.onrender.com/chat"
     resp = requests.post(BACKEND_URL, json={"message": msg})
 
     st.json(resp.json())
